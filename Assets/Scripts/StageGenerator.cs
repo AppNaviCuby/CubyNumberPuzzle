7b59f2a3239e8c37253277165605359d5bff8147
Nobuhiro Yamasaki <nobu.cacao1220@gmail.com>
2020-04-08 23:26:37 +0900

    ステージ選択ボタンを押すと、その番号のオブジェクトを生成

---
+++ Assets/Scripts/StageGenerator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageGenerator : MonoBehaviour
+{
+    public int selectedNumber = 0;
+    public GameObject[] StageGen;
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+    selectedNumber= StageController.StageNumber;
+        StageGenerate(selectedNumber);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+GameObject SelectPuzzleStage(int selectedNumber){
+       return  StageGen[selectedNumber -1];
+   
+}
+    public void StageGenerate(int selectedNumber){
+        GameObject Puzzlestage =(GameObject)Instantiate(
+            SelectPuzzleStage(selectedNumber),
+            new Vector3(0,0,0),
+            Quaternion.identity
+        ); 
+    }
+}
