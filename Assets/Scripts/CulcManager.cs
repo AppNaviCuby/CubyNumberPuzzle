48f1ae7ab411b7aeb7656b482e6ec9537a56e843
yamagi208 <scb01169@gmail.com>
2020-04-18 09:56:32 +0900

    resetボタンの追加

--- Assets/Scripts/CulcManager.cs
+++ Assets/Scripts/CulcManager.cs
@@ -17,14 +17,15 @@ public class CulcManager : MonoBehaviour
             panelList.Add(GameObject.FindGameObjectsWithTag("Line")[i]);
             for(int j=1;j<4;j++)
             {
-            panelList[i].transform.Find("Row" +j).gameObject.GetComponent<Button>().enabled = false;
+                panelList[i].transform.Find("Row" +j).gameObject.GetComponent<Image>().color = new Color(1.0f, 1.0f, 1.0f, 0.3f);
+                panelList[i].transform.Find("Row" +j).gameObject.GetComponent<Button>().enabled = false;
             }
         }
         listLongth = panelList.Count;
-        Debug.Log(listLongth);
+        //Debug.Log(listLongth);
         for(int j=1;j<4;j++)
         {
-        panelList[listLongth-1].transform.Find("Row" +j).gameObject.GetComponent<Button>().enabled = true;
+            panelList[listLongth-1].transform.Find("Row" +j).gameObject.GetComponent<Button>().enabled = true;
         }
     }
 
@@ -34,19 +35,50 @@ public class CulcManager : MonoBehaviour
         
     }
 
-    public void OnButtonClick(GameObject thisButton)
+    public void OnNumberClick(GameObject thisButton)
     {
+
+        //Debug.Log("計算開始")
+        Culclator culclator = new Culclator();
+        culclator.Culclate(thisButton);
+        //Debug.Log("計算終了");
+
         //押したButtonの親のLineの数値を取得
         int thisLine = int.Parse(Regex.Replace(thisButton.transform.parent.gameObject.name, @"[^0-9]", ""));
 
         //押した段のbuttonをオフに次の段のbuttonをオンに
         for(int j=1;j<4;j++)
         {
-        panelList[thisLine -1].transform.Find("Row" +j).gameObject.GetComponent<Button>().enabled = false;
+            panelList[thisLine -1].transform.Find("Row" +j).gameObject.GetComponent<Image>().color = new Color(1.0f, 1.0f, 1.0f, 0.3f);
+            thisButton.GetComponent<Image>().color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+            panelList[thisLine -1].transform.Find("Row" +j).gameObject.GetComponent<Button>().enabled = false;
         }
         for(int j=1;j<4;j++)
         {
-        panelList[thisLine -2].transform.Find("Row" +j).gameObject.GetComponent<Button>().enabled = true;
+            panelList[thisLine -2].transform.Find("Row" +j).gameObject.GetComponent<Button>().enabled = true;
         }
+    }
+
+    public void OnResetClick()
+    {
+        for(int i=0;i<7;i++)
+        {
+            for(int j=1;j<4;j++)
+            { 
+                panelList[i].transform.Find("Row" +j).gameObject.GetComponent<Button>().enabled = true;
+                panelList[i].transform.Find("Row" +j).gameObject.GetComponent<Image>().color = new Color(1.0f, 1.0f, 1.0f, 0.3f);
+                panelList[i].transform.Find("Row" +j).gameObject.GetComponent<Button>().enabled = false;
+            }
+        }
+        for(int j=1;j<4;j++)
+        {
+            panelList[listLongth-1].transform.Find("Row" +j).gameObject.GetComponent<Button>().enabled = true;
+        }
+        Text thisResultNumber = GameObject.Find("ResultNumber").GetComponent<Text>();
+        Text thisStartNumber = GameObject.Find("StartNumber").GetComponent<Text>();
+        thisResultNumber.text =thisStartNumber.text;
+
+        //NumberSelecter numberSelecter = new NumberSelecter();
+        //numberSelecter.ColorReset();
     }
 }
